427ec0591bd30fe18643aae18ee44f54183042c6
Laurent Muchacho <laurent.muchacho@ft.com>
2016-04-18 14:58:47 +0100

    renamed access method

--- main.js
+++ main.js
@@ -1,13 +1,14 @@
 
-const headerConfig = require('header_config.json');
+var headerConfig = require('./header_config.json');
 
 module.exports = {
-	getConfig : function(sectionName){
-		return headerConfig.navItems.map(function (obj) {
-			if (obj.name.indexOf('The Blog')>-1) {
+	setSelected : function(sectionName){
+		headerConfig.navItems.map(function (obj) {
+			if (obj.name.indexOf(sectionName)>-1) {
 				obj.selected = true;
 			}
 			return obj;
 		});
+		return headerConfig;
 	}
 };
