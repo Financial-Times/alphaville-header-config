e342f6d6f9516f25dc749a97393a1b961f273afa
Roland Vachter <roland.vachter@iquestgroup.com>
2016-05-30 16:37:12 +0300

    Differentiate logged in and logged out users, and modify the config accordingly.

--- main.js
+++ main.js
@@ -1,15 +1,20 @@
 "use strict";
 
-const headerConfig = require('./header_config.json');
+exports.get = function(selected, loggedIn) {
+	const headerConfig = require('./header_config.json');
 
-module.exports = {
-	setSelected : function(sectionName){
-		headerConfig.navItems.map(function (obj) {
-			if (obj.name.indexOf(sectionName)>-1) {
-				obj.selected = true;
-			}
-			return obj;
-		});
-		return headerConfig;
+	headerConfig.navItems.map(function (obj) {
+		if (obj.name.indexOf(selected)>-1) {
+			obj.selected = true;
+		}
+		return obj;
+	});
+
+	if (loggedIn === true) {
+		headerConfig.loggedIn = true;
+	} else {
+		headerConfig.loggedOut = true;
 	}
+
+	return headerConfig;
 };
