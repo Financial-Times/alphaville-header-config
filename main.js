b4614e2374d5f297b9b12fcc2df8ae364b8ebc0e
Roland Vachter <roland.vachter@iquestgroup.com>
2016-07-18 17:33:27 +0300

    Config adapted for the header drawer menu.

--- main.js
+++ main.js
@@ -1,28 +1,4 @@
 module.exports = {
-	toolsItems: [
-		{
-			name: "Sign In",
-			url: "https://accounts.ft.com/login",
-			liClass: "av2-logged-in-hidden",
-			attributes: "data-av2-url-referrer=\"location\" data-av2-url-referrer-value=\"current_location\""
-		},
-		{
-			name: "Subscribe",
-			url: "http://registration.ft.com/registration/subscription-service/bpsp?segid=70152",
-			liClass: "av2-logged-in-hidden"
-		},
-		{
-			name: "Sign out",
-			url: "https://accounts.ft.com/logout",
-			liClass: "av2-logged-out-hidden",
-			attributes: "data-av2-url-referrer=\"location\" data-av2-url-referrer-value=\"home_location\""
-		},
-		{
-			name: "My Account",
-			url: "https://registration.ft.com/registration/selfcare/",
-			liClass: "av2-logged-out-hidden"
-		}
-	],
 	navItems: [
 		{
 			name: "The Blog",
@@ -31,51 +7,62 @@ module.exports = {
 		{
 			name: "Markets Live",
 			url: "/marketslive",
-			lozenge: {
-				class: "ml-live-lozenge",
-				label: "LIVE"
-			}
+			lozenge: true
 		},
 		{
 			name: "The Long Room",
 			url: "/longroom"
 		}
 	],
-	desktopMeganavItems: [
+	meganavSections: [
 		{
-			name: "The Blog",
-			url: "/"
-		},
-		{
-			name: "Markets Live",
-			url: "/marketslive",
-			lozenge: {
-				class: "ml-live-lozenge",
-				label: "LIVE"
-			}
-		},
-		{
-			name: "The Long Room",
-			url: "/longroom"
+			title: "Top sections",
+			meganavSectionItems: [
+				{
+					name: "FT.com home",
+					url: "http://www.ft.com"
+				},
+				{
+					name: "The Blog",
+					url: "/",
+					expandable: true,
+					id: 'the_blog',
+					items: [
+						{
+							name: "Death of Banks",
+							url: "#"
+						},
+						{
+							name: "Alphachat",
+							url: "#"
+						},
+						{
+							name: "BitcoinMania",
+							url: "#"
+						},
+						{
+							name: "Meet the Team",
+							url: "#"
+						},
+						{
+							name: "About FTAV",
+							url: "#"
+						}
+					]
+				},
+				{
+					name: "Markets Live",
+					url: "/marketslive",
+					lozenge: true
+				},
+				{
+					name: "The Long Room",
+					url: "/longroom"
+				}
+			]
 		}
 	],
-	mobileMeganavItems: [
-		{
-			name: "The Blog",
-			url: "/"
-		},
-		{
-			name: "Markets Live",
-			url: "/marketslive",
-			lozenge: {
-				class: "ml-live-lozenge",
-				label: "LIVE"
-			}
-		},
-		{
-			name: "The Long Room",
-			url: "/longroom"
-		},
+	userNav: [
 		{
 			name: "Sign In",
 			url: "https://accounts.ft.com/login",
@@ -97,37 +84,6 @@ module.exports = {
 			name: "My Account",
 			url: "https://registration.ft.com/registration/selfcare/",
 			liClass: "av2-logged-out-hidden"
-		}
-	],
-	meganavSections: [
-		{
-			name: "The Blog",
-			meganavSectionItems: [
-				{
-					name: "Index",
-					url: "/"
-				},
-				{
-					name: "Death of Banks",
-					url: "#"
-				},
-				{
-					name: "Alphachat",
-					url: "#"
-				},
-				{
-					name: "BitcoinMania",
-					url: "#"
-				},
-				{
-					name: "Meet the Team",
-					url: "#"
-				},
-				{
-					name: "About FTAV",
-					url: "#"
-				}
-			]
 		}
 	]
 };
